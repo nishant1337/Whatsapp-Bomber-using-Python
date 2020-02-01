be7ed103303fb61cec686b02a9795926e41b6966
nishant1337 <59385239+nishant1337@users.noreply.github.com>
2020-02-01 10:31:13 +0530

    Create Wbomber.py
    Python file.

---
+++ Wbomber.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter import *
+import sys
+from selenium import webdriver
+import os,time
+def startexe():
+    victimname = str(name.get())
+    msg = str(msgg.get())
+    num = nooo.get()
+    driver = webdriver.Chrome()
+    driver.get('https://web.whatsapp.com/')
+    #15 second for scan
+    time.sleep(15)
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(victimname))
+    user.click()
+
+    msg_box = driver.find_element_by_class_name('_13mgZ')
+
+    for i in range(num):
+        msg_box.send_keys(msg)
+        button = driver.find_element_by_class_name('_3M-N-')
+        button.click()
+#GUI
+win=tk.Tk()
+win.title("Whatsapp bomber by NISHANT")
+win.geometry("600x600")
+x=Label(win,text="YOU HAVE 15 SECONDS TO SCAN AND LOGIN").place(x=140,y=10)
+x=Label(win,text="Enter name").place(x=150,y=100)
+name=StringVar()
+namebox=Entry(win,textvariable=name).place(x=250,y=100)
+
+x=Label(win,text="Enter massage").place(x=150,y=200)
+
+msgg=StringVar()
+nmsgbox=Entry(win,textvariable=msgg).place(x=250,y=200)
+
+x=Label(win,text="No of msg").place(x=150,y=300)
+nooo=IntVar()
+nobox=Entry(win,textvariable=nooo).place(x=250,y=300)
+
+tk.Button(win,text="START BOMBING",command=startexe).place(x=250,y=390)
+
+win.mainloop()
