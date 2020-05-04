103acac9a36d17d02cc46870c0e9810791149e63
Nishant Pansuriya <nishantp1337@gmail.com>
2020-05-04 14:07:46 +0530

    bomber.py
    Updated in 2020  ( Whatsapp changed ui :( )

--- Wbomber.py
+++ Wbomber.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import *
 import sys
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import os,time
 def startexe():
     victimname = str(name.get())
@@ -14,12 +16,14 @@ def startexe():
     user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(victimname))
     user.click()
 
-    msg_box = driver.find_element_by_class_name('_13mgZ')
+    msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
 
     for i in range(num):
         msg_box.send_keys(msg)
-        button = driver.find_element_by_class_name('_3M-N-')
-        button.click()
+        msg_box.send_keys(Keys.ENTER)
+
+        # button = driver.find_element_by_class_name('_3M-N-')
+        # button.click()
 #GUI
 win=tk.Tk()
 win.title("Whatsapp bomber by NISHANT")
